744ffd87dfc81c55a4d8d078d25201a9e3e06da7
Makhdoom Safeer Muhammad <makhdoom_safeer@yahoo.com>
2023-08-14 19:42:04 +0500

    JavaScript Assignment #2

--- javascript-assignment-2/chapter-12-13/app.js
+++ javascript-assignment-2/chapter-12-13/app.js
@@ -1,27 +1,155 @@
 function task_1()
 {
-    var age = 28
-    alert("I am "+age+" years old")
-}
+    var character = prompt("Enter a character")
+    character_code = character.charCodeAt(0)
 
-var visits = 1
+    if (character_code >= 65 && character_code <= 90)
+    {
+        alert("Character is a uppercase letter")
+    }
+
+    else if (character_code >= 97 && character_code <= 122)
+    {
+        alert("Character is a lowercase letter")
+    }
+
+    else
+    {
+        alert("Plz enter a character")
+    }
+}
 
 function task_2()
 {
-    alert("You have visited this site "+ visits +" times")
-    visits = visits+1
+    var first_number = prompt("Enter first number")
+    first_number = parseInt(first_number)
+    var second_number = prompt("Enter second number")
+    second_number = parseInt(second_number)
+    
+    if (first_number > second_number)
+    {
+        alert(first_number + " " + "is larger")
+    }
+
+    else if (first_number < second_number)
+    {
+        alert(second_number + " " + "is larger")
+    }
+
+    else if (first_number === second_number)
+    {
+        alert("Both numbers are equal")
+    }
+
+    else
+    {
+        alert("Plz enter integers only")
+    }
 }
 
 function task_3()
 {
-    var birthYear = 1995
-    document.write("My birth year is ",birthYear,"<br>Data type of any declared variable is number")
+    var number = prompt("Enter number")
+    number = parseInt(number)
+
+    if (number > 0)
+    {
+        alert("Number is positive")
+    }
+
+    else if (number < 0)
+    {
+        alert("Number is negative")
+    }
+
+    else if (number === 0)
+    {
+        alert("Number is zero")
+    }
+
+    else
+    {
+        alert("Plz enter a number")
+    }
 }
 
 function task_4()
 {
-    var visitors_name = "John Doe"
-    var product_title = "T-shirt"
-    var quantity = 5
-    document.write("<strong>",visitors_name,"</strong>", " ordered ", "<strong>",quantity," ",product_title, "</strong>(s) on XYZ Clothing store")
+    var character = prompt("Enter a character")
+    
+    if (character === "A" || character === "E" || character === "I" || character === "O" || character === "U" || character === "a" || character === "e" || character === "i" || character === "o" || character === "u")
+    {
+        alert("Character is a vowel")
+    }
+
+    else
+    {
+        alert("Character is not a vowel")
+    }
+}
+
+function task_5()
+{
+    var correct_password = "Pakistan"
+    var entered_password = prompt("Enter password")
+
+    if (entered_password === "")
+    {
+        alert("Please enter your password")
+    }
+
+    else if (correct_password === entered_password)
+    {
+        alert("Correct! The password you entered matches the original password")
+    }
+
+    else
+    {
+        alert("Incorrect password")
+    }
+}
+
+function task_6()
+{
+    var greeting;
+    var hour = 13;
+
+    if (hour < 18)
+    {
+        greeting = "Good day";
+        alert(greeting)
+    }
+
+    else
+    {
+        greeting = "Good evening";
+        alert(greeting)
+    }
+    
+}
+
+function task_7()
+{
+    var time = prompt("Enter time in 24 hour format")
+    time = parseInt(time)
+
+    if (time >= 0000 && time < 1200)
+    {
+        alert("Good morning!")
+    }
+
+    else if (time >= 1200 && time < 1700)
+    {
+        alert("Good afternoon!")
+    }
+
+    else if (time >= 1700 && time < 2100)
+    {
+        alert("Good evening!")
+    }
+
+    else if (time >= 2100 && time < 2359)
+    {
+        alert("Good night!")
+    }
 }
--- javascript-assignment-2/chapter-9-11/app.js
+++ javascript-assignment-2/chapter-9-11/app.js
@@ -1,52 +1,292 @@
+function task_1()
+{
+    var city = prompt("Enter city")
+
+    if (city === "Karachi")
+    {
+        alert("Welcome to city of lights")
+    }
+}
+
+function task_2()
+{
+    var gender = prompt("Enter gender")
+    
+    if (gender === "Male")
+    {
+        alert("Good Morning Sir")
+    }
+
+    else if (gender === "Female")
+    {
+        alert("Good Morning Ma’am")
+    }
+}
+
 function task_3()
 {
-    var message = "Hello World"
-    alert(message)
+    var color = prompt("Enter color")
+    
+    if (color === "Red")
+    {
+        alert("Must Stop")
+    }
+
+    else if (color === "Yellow")
+    {
+        alert("Ready to move")
+    }
+
+    else if (color === "Green")
+    {
+        alert("Move now")
+    }
 }
 
 function task_4()
 {
-    var name = "Makhdoom Safeer Muhammad"
-    alert(name)
-    var age = "28 years old"
-    alert(age)
-    var course = "Web & Mobile Application Development"
-    alert(course)
+    var fuel = prompt("Enter remaining fuel")
+
+    if (fuel < 0.25)
+    {
+        alert("Please refill the fuel in your car")
+    }
+
+    else
+    {
+        alert("No need to worry")
+    }
 }
 
 function task_5()
 {
-    var pizza = "PIZZA"
-    alert(pizza)
-    pizza = "PIZZ"
-    alert(pizza)
-    pizza = "PIZ"
-    alert(pizza)
-    pizza = "PI"
-    alert(pizza)
-    pizza = "P"
-    alert(pizza)
+    // a
+    var a = 4;
+    if (++a === 5){
+        alert("given condition for variable a is true");
+    }
+
+    // b
+    var b = 82;
+    if (b++ === 83)
+    {
+        alert("given condition for variable b is true");
+    }
+
+    // c
+    var c = 12;
+    if (c++ === 13)
+    {
+        alert("condition 1 is true");
+    }
+
+    if (c === 13)
+    {
+        alert("condition 2 is true");
+    }
+
+    if (++c < 14)
+    {
+        alert("condition 3 is true");
+    }
+
+    if(c === 14)
+    {
+        alert("condition 4 is true");
+    }
+
+    // d
+    var materialCost = 20000;
+    var laborCost = 2000;
+    var totalCost = materialCost + laborCost;
+    if (totalCost === laborCost + materialCost)
+    {
+        alert("The cost equals");
+    }
+
+    // e
+    if (true)
+    {
+        alert("True");
+    }
+
+    if (false)
+    {
+        alert("False");
+    }
+
+    // f
+    if("car" < "cat")
+    {
+        alert("car is smaller than cat");
+    }
+
 }
 
 function task_6()
 {
-    var email = "example@example.com"
-    alert("My email address is "+email)
+    var marks_obtained = prompt("Enter marks obtained")
+    var total_marks = prompt("Enter total marks")
+    percentage = (marks_obtained / total_marks) * 100
+    
+    if (percentage >= 80)
+    {
+        document.write("Total mark: ", total_marks, "<br>")
+        document.write("Mark obtained: ", marks_obtained, "<br>")
+        document.write("Percentage: ", percentage, "<br>")
+        document.write("Grade: A-one", "<br>")
+        document.write("Remarks: Excellent", "<br>")
+    }
+
+    else if (percentage >= 70)
+    {
+        document.write("Total mark: ", total_marks, "<br>")
+        document.write("Mark obtained: ", marks_obtained, "<br>")
+        document.write("Percentage: ", percentage, "<br>")
+        document.write("Grade: A", "<br>")
+        document.write("Remarks: Good", "<br>")
+    }
+
+    else if (percentage >= 60)
+    {
+        document.write("Total mark: ", total_marks, "<br>")
+        document.write("Mark obtained: ", marks_obtained, "<br>")
+        document.write("Percentage: ", percentage, "<br>")
+        document.write("Grade: B", "<br>")
+        document.write("Remarks: You need to improve", "<br>")
+    }
+
+    else
+    {
+        document.write("Total mark: ", total_marks, "<br>")
+        document.write("Mark obtained: ", marks_obtained, "<br>")
+        document.write("Percentage: ", percentage, "<br>")
+        document.write("Grade: Fail", "<br>")
+        document.write("Remarks: Sorry", "<br>")
+    }
 }
 
 function task_7()
 {
-    var book = "A smarter way to learn JavaScript"
-    alert("I am trying to learn from the Book "+book)
+    var guess_secret_number = prompt("Enter secret number")
+    guess_secret_number = parseInt(guess_secret_number)
+    var secret_number = 6
+
+    if (guess_secret_number === secret_number)
+    {
+        alert("Bingo! Correct answer")
+    }
+
+    else if (++guess_secret_number === secret_number)
+    {
+        alert("Close enough to the correct answer")
+    }
+
+    else
+    {
+        alert("Try again")
+    }
 }
 
 function task_8()
 {
-    document.write("Yah! I can write HTML content through JavaScript")
+    var number = prompt("Enter number")
+    number = parseInt(number)
+
+    if (number%3 === 0)
+    {
+        alert("Number is divisible by 3")
+    }
+
+    else
+    {
+        alert("Number is not divisible by 3")
+    }
 }
 
 function task_9()
 {
-    alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
-    document.write("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+    var number = prompt("Enter number")
+    number = parseInt(number)
+
+    if (number%2 === 0)
+    {
+        alert("Number is even")
+    }
+
+    else
+    {
+        alert("Number is odd")
+    }
+}
+
+function task_10()
+{
+    var temperature = prompt("Enter temperature")
+    temperature = parseInt(temperature)
+
+    if (temperature > 40)
+    {
+        alert("It is too hot outside.")
+    }
+
+    else if (temperature > 30)
+    {
+        alert("The Weather today is Normal.")
+    }
+
+    else if (temperature > 20)
+    {
+        alert("Today's Weather is cool.")
+    }
+
+    else if (temperature > 10)
+    {
+        alert("OMG! Today's weather is so Cool.")
+    }
+
+}
+
+function task_11()
+{
+    var number_1 = prompt("First number")
+    number_1 = parseInt(number_1)
+    var number_2 = prompt("Second number")
+    number_2 = parseInt(number_2)
+    var operation = prompt("Operation (+, -, *, /, %)")
+
+    if (operation === "+")
+    {
+        result = number_1 + number_2
+        alert("The result is: " + result)
+    }
+
+    else if (operation === "-")
+    {
+        result = number_1 - number_2
+        alert("The result is: " + result)
+    }
+    
+    else if (operation === "*")
+    {
+        result = number_1 * number_2
+        alert("The result is: " + result)
+    }
+
+    else if (operation === "/")
+    {
+        result = number_1 / number_2
+        alert("The result is: " + result)
+    }
+
+    else if (operation === "%")
+    {
+        result = number_1 % number_2
+        alert("The result is: " + result)
+    }
+
+    else
+    {
+        alert("Invalid operation")
+    }
 }
