7c62011ea72fe3c6eae3a94e3c0eae0abb98c725
Makhdoom Safeer Muhammad <makhdoom_safeer@yahoo.com>
2023-09-29 22:20:00 +0500

    JS Assignment #9 added

---
+++ javascript-assignment-9/app.js
@@ -0,0 +1,202 @@
+// Numbers
+
+function one()
+{
+    document.getElementById("screen").value += "1"
+}
+
+function two()
+{
+    document.getElementById("screen").value += "2"
+}
+
+function three()
+{
+    document.getElementById("screen").value += "3"
+}
+
+function four()
+{
+    document.getElementById("screen").value += "4"
+}
+
+function five()
+{
+    document.getElementById("screen").value += "5"
+}
+
+function six()
+{
+    document.getElementById("screen").value += "6"
+}
+
+function seven()
+{
+    document.getElementById("screen").value += "7"
+}
+
+function eight()
+{
+    document.getElementById("screen").value += "8"
+}
+
+function nine()
+{
+    document.getElementById("screen").value += "9"
+}
+
+function zero()
+{
+    document.getElementById("screen").value += "0"
+}
+
+function double_zero()
+{
+    document.getElementById("screen").value += "00"
+}
+
+// Operations
+
+function addition()
+{
+    var screen_length = document.getElementById("screen").value.length
+    if (
+        document.getElementById("screen").value[screen_length - 1] === "+" || 
+        document.getElementById("screen").value[screen_length - 1] === "-" ||
+        document.getElementById("screen").value[screen_length - 1] === "*" ||
+        document.getElementById("screen").value[screen_length - 1] === "/" ||
+        document.getElementById("screen").value[screen_length - 1] === ".")
+    {
+        document.getElementById("screen").value = document.getElementById("screen").value.slice(0, -1) + "+"
+    }
+
+    else if (document.getElementById("screen").value === "")
+    {
+        document.getElementById("screen").value += ""
+    }
+
+    else
+    {
+        document.getElementById("screen").value += "+"
+    }
+}
+
+function subtraction()
+{
+    var screen_length = document.getElementById("screen").value.length
+    if (
+        document.getElementById("screen").value[screen_length - 1] === "+" || 
+        document.getElementById("screen").value[screen_length - 1] === "-" || 
+        document.getElementById("screen").value[screen_length - 1] === "*" || 
+        document.getElementById("screen").value[screen_length - 1] === "/" ||
+        document.getElementById("screen").value[screen_length - 1] === ".")
+    {
+        document.getElementById("screen").value = document.getElementById("screen").value.slice(0, -1) + "-"
+    }
+
+    else if (document.getElementById("screen").value === "")
+    {
+        document.getElementById("screen").value += ""
+    }
+
+    else
+    {
+        document.getElementById("screen").value += "-"
+    }
+}
+
+function multiplication()
+{
+    var screen_length = document.getElementById("screen").value.length
+    if (
+        document.getElementById("screen").value[screen_length - 1] === "+" || 
+        document.getElementById("screen").value[screen_length - 1] === "-" ||
+        document.getElementById("screen").value[screen_length - 1] === "*" ||
+        document.getElementById("screen").value[screen_length - 1] === "/" ||
+        document.getElementById("screen").value[screen_length - 1] === ".")
+    {
+        document.getElementById("screen").value = document.getElementById("screen").value.slice(0, -1) + "*"
+    }
+
+    else if (document.getElementById("screen").value === "")
+    {
+        document.getElementById("screen").value += ""
+    }
+
+    else
+    {
+        document.getElementById("screen").value += "*"
+    }
+}
+
+function division()
+{
+    var screen_length = document.getElementById("screen").value.length
+    if (
+        document.getElementById("screen").value[screen_length - 1] === "+" || 
+        document.getElementById("screen").value[screen_length - 1] === "-" ||
+        document.getElementById("screen").value[screen_length - 1] === "*" ||
+        document.getElementById("screen").value[screen_length - 1] === "/" ||
+        document.getElementById("screen").value[screen_length - 1] === ".")
+    {
+        document.getElementById("screen").value = document.getElementById("screen").value.slice(0, -1) + "/"
+    }
+
+    else if (document.getElementById("screen").value === "")
+    {
+        document.getElementById("screen").value += ""
+    }
+
+    else
+    {
+        document.getElementById("screen").value += "/"
+    }
+}
+
+function decimal()
+{
+    var screen_length = document.getElementById("screen").value.length
+    if (
+        document.getElementById("screen").value[screen_length - 1] === "+" || 
+        document.getElementById("screen").value[screen_length - 1] === "-" ||
+        document.getElementById("screen").value[screen_length - 1] === "*" ||
+        document.getElementById("screen").value[screen_length - 1] === "/" ||
+        document.getElementById("screen").value[screen_length - 1] === ".")
+    {
+        document.getElementById("screen").value = document.getElementById("screen").value.slice(0, -1) + "."
+    }
+
+    else
+    {
+        document.getElementById("screen").value += "."
+    }
+}
+
+// Screen operations
+
+function clr()
+{
+    document.getElementById("screen").value = ""
+}
+
+function del()
+{
+    document.getElementById("screen").value = document.getElementById("screen").value.slice(0, -1)
+}
+
+function answer()
+{
+    var screen_length = document.getElementById("screen").value.length
+    for (var i = 0; i < screen_length; i++)
+    {
+        if (
+            document.getElementById("screen").value[screen_length - 1] === "+" || 
+            document.getElementById("screen").value[screen_length - 1] === "-" ||
+            document.getElementById("screen").value[screen_length - 1] === "*" ||
+            document.getElementById("screen").value[screen_length - 1] === "/")
+        {
+            document.getElementById("screen").value = document.getElementById("screen").value.slice(0, -1)
+        }
+    }
+    document.getElementById("screen").value = eval(document.getElementById("screen").value)
+}
