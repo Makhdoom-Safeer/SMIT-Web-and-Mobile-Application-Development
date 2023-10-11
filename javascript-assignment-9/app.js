1a80d40f1c31955fb0c4bf753b7a59ff1b0c1a4d
Makhdoom Safeer Muhammad <makhdoom_safeer@yahoo.com>
2023-10-11 21:33:50 +0500

    JS Assignment #9 updated

--- javascript-assignment-9/app.js
+++ javascript-assignment-9/app.js
@@ -15,46 +15,60 @@ function violet()
 function indigo()
 {
     document.getElementById("text").style.backgroundColor = "indigo"
+    document.getElementById("text").style.color = "white"
 }
 
 function blue()
 {
     document.getElementById("text").style.backgroundColor = "blue"
+    document.getElementById("text").style.color = "white"
 }
 
 function green()
 {
     document.getElementById("text").style.backgroundColor = "green"
+    document.getElementById("text").style.color = "white"
 }
 
 function yellow()
 {
     document.getElementById("text").style.backgroundColor = "yellow"
+    document.getElementById("text").style.color = "black"
 }
 
 function orange()
 {
     document.getElementById("text").style.backgroundColor = "orange"
+    document.getElementById("text").style.color = "white"
 }
 
 function red()
 {
     document.getElementById("text").style.backgroundColor = "red"
+    document.getElementById("text").style.color = "white"
 }
-
 
 // Output
 function post()
 {
-    if (document.getElementById("text").value !== "")
+    var text = document.getElementById("text").value
+    var text_color = document.getElementById("text").style.color
+    var text_background_color = document.getElementById("text").style.backgroundColor
+    
+    if (text !== "")
     {
-        document.getElementById("app").style.display = "none"
-        document.getElementById("output").style.backgroundColor = document.getElementById("text").style.backgroundColor
-        document.getElementById("output-text").innerHTML = document.getElementById("text").value
+        document.getElementById("output-container").innerHTML += 
+        `
+        <div class="col-12" id="output">
+            <div id="user">
+                <img src="images/user.png" alt="" width="60px" id="user-image">
+                <p id="user-name">Makhdoom Safeer Muhammad</p>
+            </div>
+        
+            <div id="output-text">
+                <p style="background-color: ${text_background_color}; color: ${text_color}">${text}</p>
+            </div>
+        </div>
+        `
     }
-}
-
-function pulse()
-{
-    document.getElementById("ugly").className = "animate__animated animate__bounce"
 }
