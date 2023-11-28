8e48a930fc4af6eb28f4cf253b4be79a296a477f
Makhdoom Safeer Muhammad <makhdoom_safeer@yahoo.com>
2023-11-28 15:04:56 +0500

    Converted to PWA

--- javascript-assignment-7/serviceWorker.js
+++ javascript-assignment-7/serviceWorker.js
@@ -4,15 +4,14 @@ const assets = [
   "/index.html",
   "/css/style.css",
   "/js/app.js",
-  "/images/coffee1.jpg",
-  "/images/coffee2.jpg",
-  "/images/coffee3.jpg",
-  "/images/coffee4.jpg",
-  "/images/coffee5.jpg",
-  "/images/coffee6.jpg",
-  "/images/coffee7.jpg",
-  "/images/coffee8.jpg",
-  "/images/coffee9.jpg",
+  "/images/icons/icon-72x72.png",
+  "/images/icons/icon-96x96.png",
+  "/images/icons/icon-128x128.png",
+  "/images/icons/icon-144x144.png",
+  "/images/icons/icon-152x152.png",
+  "/images/icons/icon-192x192.png",
+  "/images/icons/icon-384x384.png",
+  "/images/icons/icon-512x512.png",
 ]
 
 self.addEventListener("install", installEvent => {
